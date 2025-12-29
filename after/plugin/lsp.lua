ab6d52a1417f45241e525e38c1db972789c395bc
Eduardo Schiavo <eduardo.schiavo@contentsquare.com>
2025-12-29 15:28:50 +0100

    edit fmt shortcut

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,7 +19,7 @@ local lsp_attach = function(client, bufnr)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<leader>fmt', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end
 
